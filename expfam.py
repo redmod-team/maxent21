fd1633818e9e708962cc28a2fc9780fc4bb85be6
Christopher Albert <albert@alumni.tugraz.at>
2021-06-05 22:31:41 +0200

    Exponential family

--- expfam.py
+++ expfam.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import gamma
 
-x = np.linspace(-2, 2, 101)
+sq2 = np.sqrt(2.0)
+x = np.linspace(-3, 3, 101)
 th = np.linspace(1, 3, 5)
 
 def p(x, th):
-    return np.exp(-np.abs(x)**(th))
+    return np.exp(-np.abs(x/sq2)**(th))/(2.0*sq2*gamma(1.0 + 1.0/th))
 
 plt.figure()
 for thk in th:
